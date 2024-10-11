ce836e946c012093389a6051c90da358177d91a9
Harsh_Diyora <146904128+Harsh90904@users.noreply.github.com>
2024-10-11 16:19:37 +0530

    Update User.router.js

--- routers/User.router.js
+++ routers/User.router.js
@@ -1,6 +1,5 @@
 const { Router } = require("express")
 const { signup, login } = require("../controllers/User.controllers")
-
 const userRouter = Router()
 userRouter.post("/signup",signup)
 userRouter.post("/login",login)
